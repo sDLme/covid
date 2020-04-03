3f9439c25efae07acd12cb13082fd3230021ba5f
sDLme <marina2606@gmail.com>
2020-04-03 14:46:38 +0300

    add material & create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: 'classes', loadChildren: () => import('./classes/classes.module').then(m => m.ClassesModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/classes/classes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClassesRoutingModule } from './classes-routing.module';
+import { ClassesComponent } from './classes.component';
+
+
+@NgModule({
+  declarations: [ClassesComponent],
+  imports: [
+    CommonModule,
+    ClassesRoutingModule
+  ]
+})
+export class ClassesModule { }
